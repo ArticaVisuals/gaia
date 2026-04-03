00eb370ecf6f77ab725fd7ba56eea447979e1f9c
Micah Hoang <micahhoang@11-eepclapcq.Artcenter.edu>
2026-04-03 11:28:58 -0700

    Publish latest ActivityEvent Swift update and design-system HTML

--- GaiaNative/Models/ActivityEvent.swift
+++ GaiaNative/Models/ActivityEvent.swift
@@ -8,4 +8,25 @@ struct ActivityEvent: Identifiable, Codable, Hashable {
     let timestampLabel: String
     let actionLabel: String?
     let categoryIDs: [String]?
+    let thumbnailAssetName: String?
+
+    init(
+        id: String,
+        groupLabel: String?,
+        title: String,
+        subtitle: String,
+        timestampLabel: String,
+        actionLabel: String?,
+        categoryIDs: [String]?,
+        thumbnailAssetName: String? = nil
+    ) {
+        self.id = id
+        self.groupLabel = groupLabel
+        self.title = title
+        self.subtitle = subtitle
+        self.timestampLabel = timestampLabel
+        self.actionLabel = actionLabel
+        self.categoryIDs = categoryIDs
+        self.thumbnailAssetName = thumbnailAssetName
+    }
 }
